a11766ec93beb9702091ce18578adcae9d18e1c9
shoko <shoko.my0@gmail.com>
2021-03-17 23:45:35 +0900

    Add Employee show

--- src/controllers/employees/EmployeesShowServlet.java
+++ src/controllers/employees/EmployeesShowServlet.java
@@ -1,33 +1,36 @@
 package controllers.employees;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class EmployeesShowServlet
- */
+import models.Employee;
+import utils.DBUtil;
+
 @WebServlet("/employees/show")
 public class EmployeesShowServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private static final long serialVersionUID = 1L;
+
     public EmployeesShowServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Employee e = em.find(Employee.class, Integer.parseInt(request.getParameter("id")));
+        em.close();
+
+        request.setAttribute("employee", e);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/show.jsp");
+        rd.forward(request, response);
+    }
 
 }
